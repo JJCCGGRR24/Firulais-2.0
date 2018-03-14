f8c2213dd80083ca52586fa1773a37b3b439363e
JJCCGGRR24 <josecarlos.jcgr24@gmail.com>
2018-03-14 13:48:00 +0100

    Intentando mis tests

---
+++ Acme-Rendezvous/src/test/java/usecases/Test_1/UseCase05_2Test.java
@@ -0,0 +1,74 @@
+
+package usecases.Test_1;
+
+import javax.transaction.Transactional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import services.ManagerService;
+import services.ServicceService;
+import utilities.AbstractTest;
+
+@ContextConfiguration(locations = {
+	"classpath:spring/junit.xml"
+})
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+public class UseCase05_2Test extends AbstractTest {
+
+	//SUT
+	@Autowired
+	private ManagerService	managerService;
+
+	@Autowired
+	private ServicceService	servicceService;
+
+
+	//An actor who is authenticated as an administrator must be able to display a dashboard 
+	//with the following information:
+	//		The best-selling services.
+	//		The managers who provide more services than the average.
+	//		The managers who have got more services cancelled.
+
+	@Test
+	public void testDisplayDashboardByAdmin() {
+
+		super.authenticate("admin");
+
+		//Se intenta acceder a las queries del dashboard pertenecientes al caso de uso 6.2
+		//como el autenticado es admin se le permite el acceso a dichas queries
+		this.servicceService.queryNewC1B4();
+		this.managerService.queryNewC2();
+		this.managerService.queryNewC3();
+		super.unauthenticate();
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testDisplayDashboardByUser() {
+
+		//Se intenta acceder a las queries del dashboard pertenecientes al caso de uso 6.2
+		//como el autenticado es user1 no se le permite el acceso a dichas queries
+		super.authenticate("user1");
+		this.servicceService.queryNewC1B4();
+		this.managerService.queryNewC2();
+		this.managerService.queryNewC3();
+		super.unauthenticate();
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testDisplayDashboardByManager() {
+
+		//Se intenta acceder a las queries del dashboard pertenecientes al caso de uso 6.2
+		//como el autenticado es manager1 no se le permite el acceso a dichas queries
+		super.authenticate("manager1");
+		this.servicceService.queryNewC1B4();
+		this.managerService.queryNewC2();
+		this.managerService.queryNewC3();
+		super.unauthenticate();
+	}
+
+}
--- Acme-Rendezvous/src/test/java/usecases/Test_2/UseCase06_2Test.java
+++ Acme-Rendezvous/src/test/java/usecases/Test_2/UseCase06_2Test.java
@@ -1,6 +1,10 @@
 
 package usecases.Test_2;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.junit.Test;
@@ -9,9 +13,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import services.ManagerService;
+import services.RendezvousService;
 import services.ServicceService;
 import utilities.AbstractTest;
+import domain.Category;
+import domain.Rendezvous;
+import domain.Servicce;
 
 @ContextConfiguration(locations = {
 	"classpath:spring/junit.xml"
@@ -22,53 +29,60 @@ public class UseCase06_2Test extends AbstractTest {
 
 	//SUT
 	@Autowired
-	private ManagerService	managerService;
+	private RendezvousService	rendezvousService;
 
 	@Autowired
-	private ServicceService	servicceService;
+	private ServicceService		servicceService;
 
 
-	//An actor who is authenticated as an administrator must be able to display a dashboard 
-	//with the following information:
-	//		The best-selling services.
-	//		The managers who provide more services than the average.
-	//		The managers who have got more services cancelled.
+	//Create a rendezvous, which he’s implicitly assumed to attend. 
+	//	Note that a user may edit his or her rendezvouses as long as they aren’t saved 
+	//	them in final mode. Once a rendezvous is saved in final mode, it cannot be edited 
+	//	or deleted by the creator.
 
 	@Test
-	public void testDisplayDashboardByAdmin() {
+	public void driver() {
+		final Object testingData[][] = {
+			{
+				//accedemos a crear categoria con admin, no debe de haber problema pues es el unico que puede
+				//crear categorias
+				"user1", "name1", "description1", "11/12/2018 20:00", "https://forum.linkinpark.com/t/new-linkin-park-logo-what-do-you-think/31363", null
+			}, {
+				//accedemos a crear categoria con user, debería de saltar la excepción pues esto es ilegal
+				"user1", "name1", "description1", "11/12/2017 20:00", "https://forum.linkinpark.com/t/new-linkin-park-logo-what-do-you-think/31363", null
+			}
+		};
 
-		super.authenticate("admin");
+		for (int i = 0; i < testingData.length; i++)
+			this.template((String) testingData[i][0], (String) testingData[i][1], (String) testingData[i][2], super.getEntityId((String) testingData[i][4]), super.getEntityId((String) testingData[i][4]), (Class<?>) testingData[i][5]);
 
-		//Se intenta acceder a las queries del dashboard pertenecientes al caso de uso 6.2
-		//como el autenticado es admin se le permite el acceso a dichas queries
-		this.servicceService.queryNewC1B4();
-		this.managerService.queryNewC2();
-		this.managerService.queryNewC3();
-		super.unauthenticate();
 	}
 
-	@Test(expected = IllegalArgumentException.class)
-	public void testDisplayDashboardByUser() {
+	protected void template(final String username, final String name, final String description, final Date moment, final String picture, final Class<?> expected) {
 
-		//Se intenta acceder a las queries del dashboard pertenecientes al caso de uso 6.2
-		//como el autenticado es user1 no se le permite el acceso a dichas queries
-		super.authenticate("user1");
-		this.servicceService.queryNewC1B4();
-		this.managerService.queryNewC2();
-		this.managerService.queryNewC3();
-		super.unauthenticate();
+		Class<?> caught;
+
+		caught = null;
+		try {
+			//me autentico 
+			super.authenticate(username);
+
+			//creo rendezvous
+			final Rendezvous r = this.rendezvousService.create();
+
+			//introduzco datos a la categoria
+			r.setName(name);
+			r.setDescription(description);
+			r.setMoment(moment);
+			r.setPicture(picture);
+
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		}
+
+		super.checkExceptions(expected, caught);
 	}
 
-	@Test(expected = IllegalArgumentException.class)
-	public void testDisplayDashboardByManager() {
 
-		//Se intenta acceder a las queries del dashboard pertenecientes al caso de uso 6.2
-		//como el autenticado es manager1 no se le permite el acceso a dichas queries
-		super.authenticate("manager1");
-		this.servicceService.queryNewC1B4();
-		this.managerService.queryNewC2();
-		this.managerService.queryNewC3();
-		super.unauthenticate();
-	}
 
 }
